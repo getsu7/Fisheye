3b1e06c25322ab2a54d97db1e92138979070b398
Nathan Coin <azesseza11@gmail.com>
2024-06-05 16:04:23 +0200

    feat: fetch data from json + landing page

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,41 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement('article');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', picture);
+    img.setAttribute('alt', 'Portrait de ' + name);
+
+    const imgContainer = document.createElement('div');
+    imgContainer.append(img);
+
+    const a = document.createElement('a');
+    a.setAttribute('href', `/FishEye/photographer.html?id=${id}`);
+    a.setAttribute('aria-label', `Lien vers la page de ${name}`);
+    a.appendChild(imgContainer);
+
+    const h2 = document.createElement('h2');
+    h2.textContent = name;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = city + ', ' + country;
+
+    const p = document.createElement('p');
+    p.textContent = tagline;
+
+    const i = document.createElement('i');
+    i.textContent = price + '€/jour';
+
+    article.appendChild(a);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(i);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
